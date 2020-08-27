e6693e8683595989a94d910bfe8b825a45d23bd1
francisco souza <fsouza@users.noreply.github.com>
2020-08-27 01:00:23 -0400

    golangci: disable some linters

--- messages/messages.go
+++ messages/messages.go
@@ -4,11 +4,15 @@ import (
 	"crypto/rand"
 	"fmt"
 	"io"
+	"regexp"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/gempir/go-twitch-irc/v2"
 )
+
+var modNoticeRegexp = regexp.MustCompile(`^The moderators of this channel are: (.+)$`)
 
 type Notification struct {
 	Message      Message
@@ -23,13 +27,43 @@ func (n *Notification) Reply(msg string) {
 type Notify func(Notification)
 
 type Producer struct {
+	channel     string
 	client      *twitch.Client
 	subscribers map[string]Notify
 	mtx         sync.RWMutex
+	notices     chan twitch.NoticeMessage
+}
+
+type User struct {
+	DisplayName string
+	name        string
+	producer    *Producer
+}
+
+func (u *User) IsMod() (bool, error) {
+	const timeout = 500 * time.Millisecond
+	u.producer.say("/mods")
+	select {
+	case notice := <-u.producer.notices:
+		m := modNoticeRegexp.FindStringSubmatch(notice.Message)
+		mods := strings.Split(m[1], ",")
+		for _, mod := range mods {
+			if u.name == strings.TrimSpace(mod) {
+				return true, nil
+			}
+		}
+		return false, nil
+	case <-time.After(timeout):
+		return false, fmt.Errorf("couldn't determine if user is mod in %s", timeout)
+	}
+}
+
+func (u *User) IsStreamer() bool {
+	return u.name == u.producer.channel
 }
 
 type Message struct {
-	User      string
+	User      User
 	Text      string
 	Timestamp time.Time
 }
@@ -42,10 +76,20 @@ type ProducerOptions struct {
 
 func NewProducer(options *ProducerOptions) *Producer {
 	producer := &Producer{
+		channel:     options.Channel,
 		client:      twitch.NewClient(options.UserName, options.OAuthToken),
 		subscribers: map[string]Notify{},
+		notices:     make(chan twitch.NoticeMessage),
 	}
-	producer.client.OnPrivateMessage(func(msg twitch.PrivateMessage) { producer.privateMessageCb(msg) })
+	producer.client.OnPrivateMessage(func(msg twitch.PrivateMessage) {
+		producer.privateMessageCb(msg)
+	})
+	producer.client.OnNoticeMessage(func(msg twitch.NoticeMessage) {
+		select {
+		case producer.notices <- msg:
+		case <-time.After(time.Second):
+		}
+	})
 	producer.client.Join(options.Channel)
 	return producer
 }
@@ -53,7 +97,11 @@ func NewProducer(options *ProducerOptions) *Producer {
 func (p *Producer) privateMessageCb(msg twitch.PrivateMessage) {
 	notification := Notification{
 		Message: Message{
-			User:      msg.User.DisplayName,
+			User: User{
+				DisplayName: msg.User.DisplayName,
+				name:        msg.User.Name,
+				producer:    p,
+			},
 			Text:      msg.Message,
 			Timestamp: msg.Time,
 		},
@@ -91,4 +139,9 @@ func (p *Producer) Unsubscribe(id string) {
 
 func (p *Producer) Start() error {
 	return p.client.Connect()
+}
+
+func (p *Producer) say(msg string) {
+	p.client.Say(p.channel, msg)
+	fmt.Printf("said %q\n", msg)
 }
